6a5d7b10197d49b09de530b857197cbcd0fdee0d
olga-kuriatnyk <olhak@uw.edu>
2022-03-06 22:04:26 -0800

    add details screen implementation

--- app/src/main/java/com/olgak/flixster/MovieAdapter.kt
+++ app/src/main/java/com/olgak/flixster/MovieAdapter.kt
@@ -1,16 +1,19 @@
 package com.olgak.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 private const val TAG = "MovieAdapter"
+ const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -21,7 +24,7 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
         return  ViewHolder(view)
     }
 
-    // Cheap: simply bind data to an existing viewholder
+    // Cheap: simply bind data to an existing view holder
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.i(TAG, "onBindViewHolder position $position")
         val movie = movies[position]
@@ -30,16 +33,34 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    //  View.OnClickListener is interface for click listener
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
 
+        // register the click listener
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+
+        override fun onClick(v: View?) {
+            // get notified of the partifular movie which was clicked on
+            val movie = movies[adapterPosition]
+
+            // Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show() // to check if we register click listener on the appropriate row
+
+            // use the intent system to navigate ot the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 
